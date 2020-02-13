460066f5820089a41be93c4c954ab8107826ec7d
Jonas Reucher <jonasreucher@stud.h-da.de>
2020-02-13 12:51:05 +0100

    Fixed some spelling mistakes Fixed a bug that made it possible to enter new actors via changeactor Added the possibility to abort changeActor Added shutdown prints, to be used for writing warmstart files later on

--- fightOrder.py
+++ fightOrder.py
@@ -6,10 +6,7 @@ import sys
 class Game():
 
     def __init__(self):
-        self.actors = {
-            
-        }
-
+        self.actors = dict()
         self.acting = list()
 
         self.running = False
@@ -18,9 +15,9 @@ class Game():
         self.round = 0
 
     def formatNames(self, names):
-        
+
         if len(names) < 1:
-            return "EMPTY"
+            return "Nobody"
         if len(names) == 1:
             return names[0]
         else:
@@ -39,7 +36,7 @@ class Game():
         try:
             result = int(input(text))
         except ValueError:
-            print("Please enter an Integer!")
+            print("Please enter an integer!")
             result = self.inputInteger(text)
         return result
 
@@ -49,9 +46,13 @@ class Game():
         print("\n\n\n")
         done = False
         while not done:
-            inp = input("Who do you want to change?\n")
-            print(inp)
-            newInit = input("Enter new Initiative.\n")
+            inp = input("Who do you want to change? Leave blank to abort\n")
+            if inp.lstrip() == "":
+                break
+            if inp not in self.actors:
+                print ("{} is not an actor. Please enter a valid actor or add the actor first!".format(inp))
+                continue
+            newInit = input("Enter new initiative for {}.\n".format(inp))
             try:
                 val = int(newInit)
                 self.actors[inp] = val
@@ -61,18 +62,18 @@ class Game():
         return
 
     def addActor(self):
-        inp = input("Enter name of new Actor! Leave Blank to abort.\n")
+        inp = input("Enter name of new actor! Leave blank to abort.\n")
 
         if inp.lstrip() == "":
             return
         else:
             name = inp
-            value = self.inputInteger("Enter Initiative of {}\n".format(name))
+            value = self.inputInteger("Enter initiative of {}\n".format(name))
             self.actors[name] = value
 
     def removeActor(self):
         self.showStats()
-        inp = input("Who do you want to remove? Leave Blank to abort\n")
+        inp = input("Who do you want to remove? Leave blank to abort\n")
         if inp.lstrip() == "":
             return
         try:
@@ -90,8 +91,8 @@ class Game():
         print("\n\n\n\n\n")
 
     def nextRound(self):
-        if len (self.actors) < 1:
-            print ("No Actors! Please Add Actors before you start!")
+        if len(self.actors) < 1:
+            print("No actors! Please add actors before you start!")
             return
         self.round += 1
         acting = list()
@@ -99,34 +100,34 @@ class Game():
         for x in self.actors:
             if self.round % self.actors[x] == 0:
                 changed = True
-                print (self.actors[x])
+                print(self.actors[x])
                 acting.append(x)
-        self.acting = acting.copy()       
+        self.acting = acting.copy()
         if not changed:
             self.nextRound()
 
     def resetGame(self):
-        print ("Resetting Gamestate. Please Confirm!\n")
+        print("Resetting Gamestate. Please confirm!\n")
         if self.confirmInput():
             self.round = 0
         else:
-            print ("Aborting!")
+            print("Aborting!")
             return
 
     def confirmInput(self):
-        inp = input ("Are you sure? Y/N?\n")
+        inp = input("Are you sure? Y/N?\n")
         if inp.lower() == "y":
             return True
         elif inp.lower() == "n":
             return False
         else:
-            print("Invalid Input! Try Again!\n\n\n")
+            print("Invalid input! Try again!\n\n\n")
             return self.confirmInput()
-    
-    
+
     def run(self):
         while self.running:
-            inp = input("At Round {}\n\nIt's {}'s Turn\n\n\nWhat do you want to do?\n\n 1.) To Continue enter N\n 2.) To Change Stats enter C\n 3.) To remove someone enter R\n 4.) To add someone enter A\n 5.) To see the stats enter S   \n\n 6.) to reset the game enter RESET\n\n\n\n ".format(self.round,self.formatNames(self.acting)))
+            inp = input("At Round {}\n\nIt's {}'s Turn\n\n\nWhat do you want to do?\n\n 1.) To continue enter N\n 2.) To change stats enter C\n 3.) To remove someone enter R\n 4.) To add someone enter A\n 5.) To show the stats enter S   \n\n 6.) to reset the game enter RESET\n\n\n\n ".format(
+                self.round, self.formatNames(self.acting)))
 
             if inp.lower() == "n":
                 self.nextRound()
@@ -141,10 +142,13 @@ class Game():
             elif inp.lower() == "reset":
                 self.resetGame()
             else:
-                print("Invalid Input\n\n\n")
+                print("Invalid input\n\n\n")
 
 
 game = Game()
 game.running = True
-game.run()
-
+try: 
+    game.run()
+except KeyboardInterrupt:
+    print("\nPerforming shutdown")
+    print("Goodbye!")
